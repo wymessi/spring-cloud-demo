7a0a69a862c5e6acda348e4540ff144f9b25cb4c
ywang5 <ywang5@leqee.com>
2018-10-25 15:18:18 +0800

    demo of eureka-server and eureka-client

---
+++ spring-cloud-eureka-client/src/main/java/wymessi/SpringCloudEurekaClientApplication.java
@@ -0,0 +1,14 @@
+package wymessi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaClientApplication.class, args);
+    }
+}
