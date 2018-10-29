20f325cf4a135e644b82b5c715f8d06a80b12d75
ywang5 <ywang5@leqee.com>
2018-10-29 10:49:51 +0800

    Hystrix demo by Ribbon

--- spring-cloud-ribbon/src/main/java/com/wymessi/service/RibbonHelloService.java
+++ spring-cloud-ribbon/src/main/java/com/wymessi/service/RibbonHelloService.java
@@ -1,5 +1,6 @@
 package com.wymessi.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,13 @@ public class RibbonHelloService {
 
     private Logger logger = LogManager.getLogger(RibbonHelloService.class);
 
+    @HystrixCommand(fallbackMethod = "helloError")
     public String helloService(String name) {
         logger.info("开始调用pring-cloud-eureka-producer提供的hello服务");
         return restTemplate.getForObject("http://spring-cloud-eureka-producer/hello?name="+name,String.class);
 }
-
+    public String helloError(String name) {
+        logger.error("服务不可用，熔断机制生效");
+        return String.format("hello %s, sorry, this is a error", name);
+    }
 }
