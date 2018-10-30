1c775ad5df8dfcdf382f4409820a23569f306f94
ywang5 <ywang5@leqee.com>
2018-10-30 18:04:08 +0800

    demo of eureka cluster server

---
+++ spring-cloud-eureka-cluster-server/src/main/java/com/wymessi/SpringCloudEurekaClusterApplication.java
@@ -0,0 +1,14 @@
+package com.wymessi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer  //表示该应用为服务注册中心
+public class SpringCloudEurekaClusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaClusterApplication.class, args);
+    }
+}
