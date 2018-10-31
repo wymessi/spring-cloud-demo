7c43acde3fab7e80af92fac35a58db7c543ac05c
ywang5 <ywang5@leqee.com>
2018-10-31 10:39:18 +0800

    add hystrix dsahboard

--- spring-cloud-feign/src/main/java/com/wymessi/controller/FeignZipkinController.java
+++ spring-cloud-feign/src/main/java/com/wymessi/controller/FeignZipkinController.java
@@ -1,10 +1,8 @@
 package com.wymessi.controller;
 
-import com.wymessi.service.FeignHelloService;
 import com.wymessi.service.FeignZipkinService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +11,6 @@ public class FeignZipkinController {
 
     private Logger logger = LogManager.getLogger(FeignZipkinController.class);
 
-    @Autowired
     private FeignZipkinService zipkinService;
 
     @GetMapping(value = "hello/zipkin")
